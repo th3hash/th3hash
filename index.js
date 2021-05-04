67b2a269b12579adca264a81c32b04d37d0fe420
The Hash <50315422+th3hash@users.noreply.github.com>
2021-05-05 02:43:06 +0530

    A Simple Calculator

---
+++ index.js
@@ -0,0 +1,23 @@
+function add (num1, num2 ){
+    return num1 + num2;
+}
+
+function multiply (num1, num2 ){
+    return num1 * num2;
+}
+
+function divide (num1, num2 ){
+    return num1 / num2;
+}
+
+function divideR (num1, num2 ){
+    return num1 % num2;
+}
+
+function subtract (num1, num2 ){
+    return num1 - num2;
+}
+
+function calculator(num1, num2, operator){
+    return operator (num1, num2);
+}
